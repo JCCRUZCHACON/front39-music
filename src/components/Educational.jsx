3ff18c1367c8925824afc806d752b55a1a15fa8a
JCCRUZCHACON <confeccionesmc2018@gmail.com>
2024-09-26 22:33:28 -0500

    advance

---
+++ src/components/Educational.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Educational = () => {
+    return (
+        <div className="educational-purposes" >
+            APPLICATION OF SONG
+        </div>
+    );
+};
+
+export default Educational;
